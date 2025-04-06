c98f2a46cf6fcef35df93e8fd3590f2332caacb1
Jose Illan <jose_illan@hotmail.com>
2025-04-06 15:00:48 +0200

    Data validation: POST, PUT

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-# main.py
+from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+class Order(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    phone: Optional[int] = None
 
 app = FastAPI()
 
@@ -23,3 +38,18 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+# Using Request body
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/order/{order_id}")
+async def create_oder(order_id: int, order: Order):
+    return {"order_id": order_id, **order.dict()}
